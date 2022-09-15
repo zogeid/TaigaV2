7dcad174994b4a5d7bf6b2c63332278b18c79a29
dalares <zogeid@gmail.com>
2022-09-15 12:33:14 +0200

    a

--- Task.py
+++ Task.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 class Task:
     def __init__(self, task_id, ref, subject, description, us, assigned, status, init_date, fin_date, hours):
         self.task_id = task_id
@@ -11,18 +12,17 @@ class Task:
         self.status = status
         self.init_date = init_date
         self.fin_date = fin_date
-        self.hours = hours.replace(",", ".")
+        self.hours = hours
+        self.calculo_horas()
         self.url = f'https://tree.taiga.io/project/dalares-notificaciones/task/{self.ref}'
 
     def __repr__(self):
         return str(self.__dict__)
 
     def calculo_horas(self):
-        if self.hours == 0:
-            print("vacias")
-            if datetime.datetime.now().month== (7 or 8):
-                self.hours = 7
+        if self.hours == "":
+            if datetime.datetime.now().month == (7 or 8):
+                self.hours = '7'
             else:
-                self.hours = 8.5
-
-
+                self.hours = '8.5'
+        self.hours.replace(",", ".")
