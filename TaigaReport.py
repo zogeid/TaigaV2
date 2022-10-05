9d5e2bf63279c3c9948a8d21aa9cfabbeef059b2
dalares <zogeid@gmail.com>
2022-10-05 13:33:38 +0200

    a

--- TaigaReport.py
+++ TaigaReport.py
@@ -11,20 +11,19 @@ from UserStory import UserStory
 from Task import Task
 start = time.time()
 
-# path = 'C:/Users/dalares/PycharmProjects/TaigaV2'  # Ruta donde descargamos y creamos el .pdf
-# path = 'C:/Users/Popolo/PycharmProjects/TaigaV2'  # Ruta donde descargamos y creamos el .pdf
 path = ''
 
 todayF = date.today().strftime("%d-%b-%Y")  # ddmmaaaa
 epic_dict = {}
 us_dict = {}
 task_dict = {}
-# w: Week, None: all
-mode = 'w'
 
-# TITULO GENERAL, 1, 1.1, 1.1.1
+# w: Week, a: all, d: from date
+mode = 'd'
+fromDate = datetime.strptime('5/10/22', '%d/%m/%y')
 
 
+# Recuperamos las Tasks en función del MODE seleccionado
 def struc_task():
     task_url = 'https://api.taiga.io/api/v1/tasks/csv?uuid=7db9148a134947d89c13468473c193a0'
     filename = "Task" + " - " + str(todayF)
@@ -35,7 +34,12 @@ def struc_task():
         for row in struc_csv_reader:
             if struc_csv_reader.line_num > 1:
                 task = Task(row[0], row[1], row[2], row[3], row[4], row[11], row[13], row[23][2:19], row[25][2:19], row[28])
-                if mode != 'w' or (mode == 'w' and datetime.strptime(task.init_date, '%y-%m-%d %H:%M:%S') > (datetime.today() - timedelta(days=5))):
+                # Si el MODE es 'all' o es 'week'/'date' y la tarea se empezó en el rango apropiado: se añade la tarea
+                asd = fromDate - timedelta(days=5)
+                qwe = datetime.strptime(task.init_date, '%y-%m-%d %H:%M:%S') > (fromDate - timedelta(days=5))
+                if mode == 'a' or \
+                        (mode == 'w' and datetime.strptime(task.init_date, '%y-%m-%d %H:%M:%S') > (datetime.today() - timedelta(days=5))) or \
+                        (mode == 'd' and datetime.strptime(task.init_date, '%y-%m-%d %H:%M:%S') > fromDate):
                     task_dict[int(task.ref)] = task
     os.remove(filename + '.csv')
 
@@ -56,7 +60,7 @@ def struc_us():
                         us.task_dict[temp_task.ref] = temp_task
                     except KeyError:
                         pass
-                if (mode == 'w' and us.task_dict) or mode != 'w':
+                if ((mode == 'w' or mode == 'd') and us.task_dict) or mode == 'a':
                     us_dict[int(us.ref)] = us
     os.remove(filename + '.csv')
 
@@ -77,7 +81,7 @@ def struc_epic():
                         epic.us_dict[temp_us.ref] = temp_us
                     except KeyError:
                         pass
-                if (mode == 'w' and epic.us_dict) or mode != 'w':
+                if ((mode == 'w' or mode == 'd') and epic.us_dict) or mode == 'a':
                     epic_dict[epic.epic_id] = epic
     os.remove(filename + '.csv')
 
@@ -100,8 +104,10 @@ def epic_dict_printer():
     pdf.set_font("Arial", 'B', 18)
     if mode == 'w':
         pdf.multi_cell(200, 10, txt=f'INFORME DE TAREAS SEMANAL: {(datetime.today() - timedelta(days=5)).strftime("%b %d %Y")} - {datetime.today().strftime("%b %d %Y")}', align='L')
-    else:
+    elif mode == 'a':
         pdf.multi_cell(200, 10, txt=f'INFORME DE TAREAS GLOBAL', align='L')
+    elif mode == 'd':
+        pdf.multi_cell(200, 10, txt=f'INFORME DE TAREAS DESDE {(datetime.today() - timedelta(days=5)).strftime("%b %d %Y")}', align='L')
 
     pdf.multi_cell(200, 10, txt="", align='L')
     for e in epic_dict:
@@ -160,4 +166,4 @@ estructura_epic_userstory_task()
 epic_dict_printer()
 # print(json.dumps(epic_dict, default=lambda o: o.__dict__, sort_keys=True, indent=0))
 # epic_dict_printer()
-print(f'Time elapsed: ', timedelta(seconds = time.time()-start))
+print(f'Time elapsed: ', timedelta(seconds=time.time()-start))
--- test.py
+++ test.py
@@ -1,6 +1,10 @@
+from datetime import datetime, timedelta
+
 dictio = {}
 
-if not dictio:
-    print('vacio')
-else:
-    print('lleno')
+mode = '30/09/22'
+
+print(f'INFORME DE TAREAS SEMANAL: {(datetime.today() - timedelta(days=5)).strftime("%b %d %Y")}')
+datetime_object = datetime.strptime(mode, '%d/%m/%y')
+print(f'INFORME DE TAREAS SEMANAL: ', datetime_object)
+
